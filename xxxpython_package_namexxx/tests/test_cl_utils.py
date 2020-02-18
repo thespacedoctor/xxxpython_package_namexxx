05dd280328b21b373e9fdec77eba52d894367851
Dave <d.r.young@qub.ac.uk>
2020-02-18 14:49:10 +0000

    adding empty unit test for jenkins

---
+++ xxxpython_package_namexxx/tests/test_cl_utils.py
@@ -0,0 +1,66 @@
+from __future__ import print_function
+from builtins import str
+import os
+import unittest
+import shutil
+import unittest
+import yaml
+from xxxpython_package_namexxx.utKit import utKit
+from fundamentals import tools
+from os.path import expanduser
+home = expanduser("~")
+
+packageDirectory = utKit("").get_project_root()
+settingsFile = packageDirectory + "/test_settings.yaml"
+
+# settingsFile = home + "/.config/xxxpython_package_namexxx/xxxpython_package_namexxx.yaml"
+su = tools(
+    arguments={"settingsFile": settingsFile},
+    docString=__doc__,
+    logLevel="DEBUG",
+    options_first=False,
+    projectName=None,
+    defaultSettingsFile=False
+)
+arguments, settings, log, dbConn = su.setup()
+
+# SETUP AND TEARDOWN FIXTURE FUNCTIONS FOR THE ENTIRE MODULE
+moduleDirectory = os.path.dirname(__file__)
+utKit = utKit(moduleDirectory)
+log, dbConn, pathToInputDir, pathToOutputDir = utKit.setupModule()
+utKit.tearDownModule()
+
+try:
+    shutil.rmtree(pathToOutputDir)
+except:
+    pass
+# COPY INPUT TO OUTPUT DIR
+shutil.copytree(pathToInputDir, pathToOutputDir)
+
+# Recursively create missing directories
+if not os.path.exists(pathToOutputDir):
+    os.makedirs(pathToOutputDir)
+
+# xt-setup-unit-testing-files-and-folders
+
+
+class test_cl_utils(unittest.TestCase):
+
+    def test_cl_utils_function_exception(self):
+
+        from xxxpython_package_namexxx import cl_utils
+        try:
+            this = cl_utils(
+                log=log,
+                settings=settings,
+                fakeKey="break the code"
+            )
+            this.get()
+            assert False
+        except Exception as e:
+            assert True
+            print(str(e))
+
+        # x-print-testpage-for-pessto-marshall-web-object
+
+    # x-class-to-test-named-worker-function
